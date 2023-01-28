cdf674fea6c987d612f3cd2e9566a7e039db8594
Ananya Nair <an.nair1702@gmail.com>
2023-01-28 22:56:17 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+/**
+
+      Header
+
+         - Logo(Title)
+
+         - Nav Items(Right Side)
+
+         - Cart
+
+      Body 
+
+         - Search bar
+
+         - RestrauntList
+
+           - RestaurantCard (many cards)
+
+               - Image
+
+               - Name
+
+               - Rating
+
+               - Cusines
+
+      Footer
+
+       - links
+
+       - Copyright
+
+     
+
+     */
+
+const Title = () => (
+  <a href="/">
+    <img
+      className="logo"
+      alt="logo"
+      src="https://yt3.ggpht.com/ytc/AMLnZu_EC-ECXAxRAixWGEfMsE1rdSoetBHyxmLNdtCB=s900-c-k-c0x00ffffff-no-rj"
+    />
+  </a>
+);
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Title />
+
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+
+          <li>About</li>
+
+          <li>Contact</li>
+
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+
+      {/* <Body /> */}
+
+      {/* <Footer /> */}
+    </>
   );
 }
 
